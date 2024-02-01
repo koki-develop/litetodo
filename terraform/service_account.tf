4f46d7d0217e353f3a8ea44f4ca80e8bc62982b2
koki-develop <kou.pg.0131@gmail.com>
2024-02-02 07:42:38 +0900

    Add terraform service account and IAM roles

---
+++ terraform/service_account.tf
@@ -0,0 +1,15 @@
+locals {
+  app_roles = []
+}
+
+resource "google_service_account" "app" {
+  account_id = "${var.project}-app"
+}
+
+resource "google_project_iam_member" "app" {
+  for_each = toset(local.app_roles)
+
+  project = var.project
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.app.email}"
+}
