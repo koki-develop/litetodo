5b85c3e37087cc345db187beb5133c07ffb913b9
koki-develop <kou.pg.0131@gmail.com>
2024-02-02 17:44:27 +0900

    Update artifact registry repository id in Terraform

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -2,22 +2,6 @@ resource "google_artifact_registry_repository" "app" {
   depends_on = [google_project_service.main]
 
   location      = var.region
-  repository_id = "app"
-  format        = "DOCKER"
-}
-
-resource "google_artifact_registry_repository" "replicate" {
-  depends_on = [google_project_service.main]
-
-  location      = var.region
-  repository_id = "replicate"
-  format        = "DOCKER"
-}
-
-resource "google_artifact_registry_repository" "restore" {
-  depends_on = [google_project_service.main]
-
-  location      = var.region
-  repository_id = "restore"
+  repository_id = "litetodo"
   format        = "DOCKER"
 }
